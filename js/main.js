a7635785fa738ca1693f8984a369f204e90268bd
SayedAkram2003 <163974799+SayedAkram2003@users.noreply.github.com>
2024-03-22 17:50:06 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.getElementById("example").innerHTML = "hello world";
+
+document.getElementById("example").style.color = "#f00";
+document.getElementById("example").style.textAlign = "center";
+
+alert("sayed ")
+
+console.log("akram")
+
